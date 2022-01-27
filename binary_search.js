dfe139f4a2505cf47772e4a0eace498176cc0d05
Nikki <nikkster311@gmail.com>
2022-01-26 19:37:05 -0800

    binary search attempt 2

--- binary_search.js
+++ binary_search.js
@@ -1,17 +1,24 @@
 function binary_search(list, target) {
     var first = 0;
     var last = list.length - 1;
-    var index = last/2
 
-    while(index > 0) {
-        if(list[index] == target) {
-            return index
+    while(first <= 0) {
+        midpoint = (first + last)/2
+        if(list[midpoint] == target) {
+            console.log(list[midpoint]);
+            return midpoint
         // if target is in the bottom half
-        } else if(target < list[index]) {
-            index = index/2
+        } else if(target < list[midpoint]) {
+            last = midpoint - 1
         // if target is in the upper half
-        } else if(target > list[index]) {
-            index += index/2
+        } else if(target > list[midpoint]) {
+            first = midpoint + 1
         }
     }
+    return none;
 }
+
+
+list = [...Array(30).keys()];
+console.log(list);
+binary_search(list, 23);
--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,34 @@
 def binary_search(list, target):
     first = 0
     last = len(list) - 1
-    index = last/2
 
     # run while first is not 0
 
-    while first < 0:
-        if list[index] == target:
-            return index
+    while first <= last:
+        #  //2 returns a rounded up result (no dec)
+        midpoint = (first + last)//2
+        if list[midpoint] == target:
+            return midpoint
             break
         # if target is less than halfway
-        elif target < list[index]:
-            index = index/2
+        elif target < list[midpoint]:
+            last = midpoint - 1
         # if target is more than halfway 
-        elif target > list[index]:
-            index += index/2 
+        else:
+            first = midpoint + 1 
+
+    return None
+
+
+
+def verify(index):
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in list")
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10, 11, 12]
+
+result = binary_search(numbers, 12)
+verify(result)
